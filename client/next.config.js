8146403d6d40aa101e69c26c5d5bf0fa7a7c4225
Yakov Shitrit <yakovd33@gmail.com>
2021-11-21 10:31:15 +0200

    Backend linkage

--- client/next.config.js
+++ client/next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   future: { webpack5: true },
-  // env: {
-  //   API_URL: 'http://localhost:5000'
-  // }
+  env: {
+    API_URL: 'https://sahar-fruits.herokuapp.com/'
+  }
 }
