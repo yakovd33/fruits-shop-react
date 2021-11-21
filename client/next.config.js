7bafd3d4fa62271090206d187111278e322ed85d
Yakov Shitrit <yakovd33@gmail.com>
2021-11-21 10:32:48 +0200

    Backend backslash

--- client/next.config.js
+++ client/next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   future: { webpack5: true },
   env: {
-    API_URL: 'https://sahar-fruits.herokuapp.com/'
+    API_URL: 'https://sahar-fruits.herokuapp.com'
   }
 }
