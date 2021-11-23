724b465853907628db4b770eac9847eccb962187
Yakov Shitrit <yakovd33@gmail.com>
2021-11-23 17:02:11 +0200

    Backend link fixed :(

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   reactStrictMode: true,
   future: { webpack5: true },
   env: {
-    // API_URL: 'https://sahar-fruits.herokuapp.com',
-    API_URL: 'http://localhost:5000',
+    API_URL: 'https://sahar-fruits.herokuapp.com',
+    // API_URL: 'http://localhost:5000',
   }
 }
