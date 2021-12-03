50e1c00ee94fd3e544214726a594a79b6ffafee9
Yakov Shitrit <yakovd33@gmail.com>
2021-12-03 18:12:59 +0200

    .

--- client/pages/index.js
+++ client/pages/index.js
@@ -134,7 +134,8 @@ export default function Home({ cartItems, setCartItems }) {
 
 					<div id="main-products-list">
 						{ products && products.map((product) => (
-							<ProductShowcase cartItems={ cartItems } setCartItems={ setCartItems } name={ product.name } price={ product.price } salePrice={ product.salePrice } unit={ product.unitType } image={ `${process.env.API_URL}/image/${ product.id }.jpg ` }/>
+                            { product.name }
+							// <ProductShowcase cartItems={ cartItems } setCartItems={ setCartItems } name={ product.name } price={ product.price } salePrice={ product.salePrice } unit={ product.unitType } image={ `${process.env.API_URL}/image/${ product.id }.jpg ` }/>
 						)) }
 					</div>
 
