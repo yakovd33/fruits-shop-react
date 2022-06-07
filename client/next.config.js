e6ce35d932b00edcb0b7d331d1f7e09716cf9cdb
Yakov Shitrit <yakov@becode.co.il>
2022-06-07 12:01:18 +0300

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	reactStrictMode: true,
 	future: { webpack5: true },
 	env: {
-		API_URL: 'http://fruits-env.eba-3iccc3dt.eu-west-1.elasticbeanstalk.com',
+		API_URL: 'https://sahar-fruits.herokuapp.com',
 		// API_URL: "http://localhost:5000",
 	},
 };
