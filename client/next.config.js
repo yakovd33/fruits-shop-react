72f078f1c481933efe029b9f881466e6222fefdb
Yakov Shitrit <yakovd33@gmail.com>
2022-12-24 16:14:57 +0200

    Image buckets domains

--- client/next.config.js
+++ client/next.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	reactStrictMode: true,
 	future: { webpack5: true },
 	images: {
-		domains: ['eropa.co.il'],
+		domains: ['eropa.co.il', 'pryerek-product-thumbs.s3.eu-central-1.amazonaws.com', 'main-slides.s3.amazonaws.com'],
 	},
 	env: {
 		// API_URL: "http://localhost:4000",
