f7fb64aa11b05b891053bb7084c63704133f6aaa
Yakov Shitrit <yakovd33@gmail.com>
2023-10-01 14:59:42 +0300

    Test

--- client/next.config.js
+++ client/next.config.js
@@ -4,10 +4,12 @@ module.exports = {
 		remotePatterns: [{
 			protocol: 'https',
 			hostname: process.env.NEXT_PUBLIC_PRODUCT_THUMBS_PUBLIC_BUCKET,
+			unoptimized: true
 		}, {
 			protocol: 'https',
 			hostname: process.env.NEXT_PUBLIC_MAIN_SLIDER_PUBLIC_BUCKET,
-		}]
+			unoptimized: true
+		},]
 	},
 	env: {
 		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
